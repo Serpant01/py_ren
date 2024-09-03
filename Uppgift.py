cb05d3e0788f432aca5449282dc88bc1abddb911
Serpant01 <monethasilas2@gmail.com>
2024-09-03 08:32:31 +0200

    changes

---
+++ Uppgift.py
@@ -0,0 +1,6 @@
+import math
+
+bas = float(input("Ange basen:2 "))
+exponent = float(input("Ange exponenten:4 "))
+resultat = bas 2**4 exponent
+print(f"Resultatet av {bas} upphöjt till {exponent} är {resultat}")
