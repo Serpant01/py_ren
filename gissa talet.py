210f20adb968d23151aad2ef770a4c6b3f8db740
Serpant01 <monethasilas2@gmail.com>
2024-10-08 08:34:32 +0200

    done

---
+++ gissa talet.py
@@ -0,0 +1,18 @@
+import random
+hemligt_tal = random.randint(1, 100)
+max_gissningar = 7
+gissningar = 0
+print("Jag har valt ett tal mellan 1 och 100. Du har 7 försök att gissa rätt.")
+while gissningar < max_gissningar:
+    gissning = int(input("Gissa talet: "))
+    gissningar += 1
+
+    if gissning < hemligt_tal:
+        print("För lågt.")
+    elif gissning > hemligt_tal:
+        print("För högt.")
+    else:
+        print("Bra jobbat! Du gissade rätt!")
+        break
+if gissning != hemligt_tal:
+    print(f"Tyvärr, du har gissat fel 7 gånger. Det rätta talet var {hemligt_tal}.")
